972cc4e84908e2e6f942b5dd86d20a27f0d39d01
rinatuse <bastanovrinat@gmail.com>
2026-09-08 22:31:55 +0300

    update naive

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import naive from 'naive-ui'
 
 import App from './App.vue'
 import router from './router'
@@ -8,5 +9,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(naive)
 
 app.mount('#app')
